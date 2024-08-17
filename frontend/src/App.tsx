e0c47f5aca61f4a367799fa3b6ae659aa9b58728
Keenan Keeling <kkeeling@automatastudios.com>
2024-08-17 13:40:34 -0400

    feat: Add react-router-dom and implement routing setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
-import Navigation from './components/Navigation'
 import Footer from './components/Footer'
-import Home from './pages/Home'
-import Transcription from './pages/Transcription'
-import ReportGeneration from './pages/ReportGeneration'
-import NotFound from './pages/NotFound'
+import PoliceReportGenerator from './components/PoliceReportGenerator'
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-black text-white">
-        <Header />
-        <Navigation />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/transcription" element={<Transcription />} />
-            <Route path="/report-generation" element={<ReportGeneration />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen bg-black text-white">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <PoliceReportGenerator />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
